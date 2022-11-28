d2d479a4a05cf4056c4e3fb57aa211a0139895d6
batuky <imbatuhankaya@gmail.com>
2022-11-28 21:46:18 +0300

    loaded data, readed individual value and saved data but not loaded from the html file

--- first.py
+++ first.py
@@ -1,11 +1,31 @@
 from pandas_datareader import data as web
 import datetime as dt
+import pandas as pd
 
 
+#Loading data
 start = dt.datetime(2019,1,1)
 end = dt.datetime.now()
 
 df = web.DataReader( 'AAPL' , 'yahoo' , start, end)
 
-print(df)
+#print(df)
 
+
+#Reading individual values
+#it is printing just closing values
+# print(df['Close'])
+
+#it is giving daily value.
+# print(df['Close']['2022-02-14'])
+
+#it is showing fifth data
+# print (df[ 'Close' ][ 5 ])
+
+
+#saving and loading data 
+
+df.to_html( 'apple.html' )
+
+#control that func. find another alternative to read the html file
+df = pd.read_html( "apple.html" )
